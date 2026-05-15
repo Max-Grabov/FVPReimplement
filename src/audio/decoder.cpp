3feb5f923ebc3bfd7825133f714483636cc25f55
Max-Grabov <maxpgrabov@gmail.com>
2026-05-15 00:04:51 -0400

    fix: remove stupid struct and add add shit prints instead of a good logger. also fix seg fault

--- src/audio/decoder.cpp
+++ src/audio/decoder.cpp
@@ -11,20 +11,10 @@ namespace AstralAir
 namespace Audio
 {
 
-OggStreamInfo::OggStreamInfo()
-{
-  
-}
-
-OggStreamInfo::~OggStreamInfo()
-{
-  ogg_stream_clear(&stream_state);
-  ogg_sync_clear(&sync_state);
-}
-
 void DecodeOggContainer(const std::string &view)
 {
-  OggStreamInfo ogg_stream;
+  ogg_sync_state sync_state; 
+  ogg_stream_state stream_state;
   ogg_page ogg_page;
   ogg_packet ogg_packet;
   vorbis_info v_info;
@@ -32,6 +22,8 @@ void DecodeOggContainer(const std::string &view)
   vorbis_dsp_state v_dsp;
   vorbis_block v_block;
   bool stream_end{false};
+
+  ogg_sync_init(&sync_state);
 
   while(1)
   {
@@ -45,36 +37,42 @@ void DecodeOggContainer(const std::string &view)
     }
 
     long byte_size = view.size() * sizeof(char);
-    char *buffer = ogg_sync_buffer(&ogg_stream.sync_state, byte_size);
 
+    if(byte_size == 0)
+    {
+      std::cerr << "0 byte size for " << view << "\n";
+      return;
+    }
+
+    char *buffer = ogg_sync_buffer(&sync_state, byte_size);
     if(!buffer)
     {
       std::cerr << "Input buffer for decoding is empty\n";
       return;
     }
 
-    if(ogg_sync_pageout(&ogg_stream.sync_state, &ogg_page) != 1)
+    if(ogg_sync_pageout(&sync_state, &ogg_page) != 1)
     {
-      if(byte_size < (0b1 << 12))
+      if(byte_size < (1 << 12))
       {
-        // out of data
+        std::cout << "Out of data\n"; 
         break;
       }
     }
     
-    ogg_sync_wrote(&ogg_stream.sync_state, byte_size);
-    ogg_stream_init(&ogg_stream.stream_state, ogg_page_serialno(&ogg_page));
+    ogg_sync_wrote(&sync_state, byte_size);
+    ogg_stream_init(&stream_state, ogg_page_serialno(&ogg_page));
     
     vorbis_info_init(&v_info);
     vorbis_comment_init(&v_comment);
 
     // Standard check to ensure it is vorbis
-    if(ogg_stream_pagein(&ogg_stream.stream_state, &ogg_page) < 0)
+    if(ogg_stream_pagein(&stream_state, &ogg_page) < 0)
     {
       std::cerr << "Error reading first page of OggStream\n";
       return;
     }
-    if(ogg_stream_packetout(&ogg_stream.stream_state, &ogg_packet) != 1)
+    if(ogg_stream_packetout(&stream_state, &ogg_packet) != 1)
     {
       std::cerr << "Error reading initial packet of OggStream\n";
       return;
@@ -88,32 +86,41 @@ void DecodeOggContainer(const std::string &view)
     // First two packets
     for(uint8_t count{}; count < 2;)
     {
-      if(!ogg_sync_pageout(&ogg_stream.sync_state, &ogg_page)) break;
+      if(!ogg_sync_pageout(&sync_state, &ogg_page))
+      {
+        std::cout << "empty\n";
+        break;
+      }
 
-      ogg_stream_pagein(&ogg_stream.stream_state, &ogg_page);
+      ogg_stream_pagein(&stream_state, &ogg_page);
       while(count < 2)
       {
-        auto result = ogg_stream_packetout(&ogg_stream.stream_state, &ogg_packet);
-        if(!result) break;
+        auto result = ogg_stream_packetout(&stream_state, &ogg_packet);
+        if(!result) 
+        {
+          std::cout << "empty\n";
+          break;
+        }
+
         if(result < 0)
         {
-          // Corrupted
+          std::cerr << "Corrupted\n";
           return;
         }
         result=vorbis_synthesis_headerin(&v_info, &v_comment, &ogg_packet);
         if(result < 0)
         {
-          // Corrupted
+          std::cerr << "Corrupted\n";
           return;
         }
         ++count;
       } 
     }
 
-    {
-      char **pin = v_comment.user_comments;
-      while(*pin) std::cout << *pin << "\n"; ++pin;
-    }
+    char **pin = v_comment.user_comments;
+    while(*pin) std::cout << *pin << "\n"; ++pin;
+
+    std::cout << "channel " << v_info.channels << " rate " << v_info.rate  << "\n";
   }
 }
 
--- src/audio/decoder.hpp
+++ src/audio/decoder.hpp
@@ -6,15 +6,6 @@ namespace AstralAir
 namespace Audio
 {
 
-struct OggStreamInfo
-{
-  ogg_sync_state sync_state; 
-  ogg_stream_state stream_state;
-
-  OggStreamInfo();
-  ~OggStreamInfo();
-};
-
 void DecodeOggContainer(const std::string &view);
 
 } // namespace Audio
